9d99e22f7ed2855cf7b0e4ea0bf566f0d6a05d4d
Daniel Walz <kungwaz@gmail.com>
2015-11-15 12:58:37 +0100

    Updated tracks service to use post.

--- frontend/src/modules/pad-thai/services/tracksService.js
+++ frontend/src/modules/pad-thai/services/tracksService.js
@@ -8,26 +8,50 @@
   TracksService.$inject = [
     '$http',
     '$q',
-    'pad-thai.config.environment'
+    'pad-thai.config.environment',
+    'pad-thai.config.weather-types',
+    'pad-thai.services.GenresService'
   ];
 
-  function TracksService($http, $q, environment) {
+  function TracksService($http, $q, environment, weatherConstants, GenresService) {
     function getTracksForCoordinates(lat, lng, mood) {
       var defered = $q.defer();
 
-      $http.get(environment.apiUrl + ':' + environment.apiPort + '/tracks?lat=' + lat + '&lng=' + lng + '&mood=' + mood).then(
-        function success(response){
-          defered.resolve({
-            weatherType: response.data.weatherType,
-            tracks: response.data.tracks,
-            weatherData: response.data.weatherData
-          });
-        },
-        function error(response){
-          defered.reject();
-        }
+      var weatherTypes = weatherConstants.types;
+      var genreWeatherMap = [];
+
+      for(var i = 0; i < weatherTypes.length; i++) {
+        genreWeatherMap.push(weatherTypes[i]);
+        genreWeatherMap[weatherTypes[i]] = GenresService.getSelectedGenreByWeatherType(weatherTypes[i]);
+      }
+
+      $http.post(environment.apiUrl + ':' + environment.apiPort + '/tracks?lat=' + lat + '&lng=' + lng + '&mood=' + mood, genreWeatherMap).then(
+          function success(response){
+            defered.resolve({
+              weatherType: response.data.weatherType,
+              tracks: response.data.tracks,
+              weatherData: response.data.weatherData
+            });
+          },
+          function error(response){
+            defered.reject();
+          }
       );
       return defered.promise;
+
+      //$http.get(environment.apiUrl + ':' + environment.apiPort + '/tracks?lat=' + lat + '&lng=' + lng + '&mood=' + mood).then(
+      //  function success(response){
+      //    defered.resolve({
+      //      weatherType: response.data.weatherType,
+      //      tracks: response.data.tracks,
+      //      weatherData: response.data.weatherData
+      //    });
+      //  },
+      //  function error(response){
+      //    defered.reject();
+      //  }
+      //);
+      //return defered.promise;
     }
 
     return {
